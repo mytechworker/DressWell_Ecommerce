c8ef4ff08c0111e148dc2594db8330a7cd6f14f2
pratima-olixlab <olixlab.22@gmail.com>
2024-04-10 16:35:02 +0530

    changes in orders component

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -6,7 +6,7 @@ export class TimestampToDatePipe implements PipeTransform {
   transform(timestamp: any): string | null {
     if (timestamp && timestamp.seconds && typeof timestamp.seconds === 'number') {
       const datePipe = new DatePipe('en-US');
-      return datePipe.transform(timestamp.seconds * 1000, 'medium'); // Convert seconds to milliseconds
+      return datePipe.transform(timestamp.seconds * 1000, 'medium');
     }
     return null;
   }
